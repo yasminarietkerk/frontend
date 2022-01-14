d143bee005112a466792358f5c6ef317181a7974
yasminarietkerk <94236209+yasminarietkerk@users.noreply.github.com>
2022-01-14 19:08:53 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
 // JavaScript Document
 var button = document.querySelector("nav button");
+var vrachtwagen = document.querySelector("section:first-of-type h2");
 
 function toonMenu(event) {
     console.log(event);
     deNav = event.target.parentNode;
-    deNav.classList.toggle("toonMenu");
+    document.body.classList.toggle("toonMenu");
+}
+
+function toeteren() {
+    var vrachtwagenGeluid = document.querySelector("audio");
+    vrachtwagenGeluid.play();
+
 }
 
 button.addEventListener("click", toonMenu);
+vrachtwagen.addEventListener("click", toeteren);
